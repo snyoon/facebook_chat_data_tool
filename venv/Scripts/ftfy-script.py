310e8da1ad2102b2c3894af4640629118c14119e
snyoon <justinyoon23@gmail.com>
2020-01-19 04:38:20 -0800

    config file wont change

---
+++ venv/Scripts/ftfy-script.py
@@ -0,0 +1,12 @@
+#!F:\Justin\Documents\facebook_chat_data_tool\venv\Scripts\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'ftfy==5.6','console_scripts','ftfy'
+__requires__ = 'ftfy==5.6'
+import re
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(
+        load_entry_point('ftfy==5.6', 'console_scripts', 'ftfy')()
+    )
